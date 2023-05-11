10b64a63ecbaae06df124ae5b8afa082c16b13a9
gualbysoca <telco.gmm@gmail.com>
2023-05-11 05:05:12 -0400

    logger added

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
 const { exec } = require('child_process');
 const { body, validationResult } = require('express-validator');
+const winston = require('winston');
 
 const app = express();
+
+// Create logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: process.env.LOG_FILE_PATH })
+    ]
+});
 
 app.use(bodyParser.json());
 
@@ -21,8 +31,8 @@ app.post('/send_sms',
         .isLength({ min: 11, max: 11 })
         .notEmpty()
         .custom((value) => {
-            if (!value.startsWith('591')) {
-                throw new Error('destination_number must start with 591');
+            if (!value.startsWith('+591')) {
+                throw new Error('destination_number must start with +591');
             }
             return true;
         }),
@@ -32,6 +42,7 @@ app.post('/send_sms',
     (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
+            logger.error(`Validation Error: ${JSON.stringify(errors.array())}`);
             return res.status(400).json({ errors: errors.array() });
         }
 
@@ -39,13 +50,15 @@ app.post('/send_sms',
         const message = req.body.message;
 
         if (!process.env.GAMMU_COMMAND) {
+            logger.error('GAMMU_COMMAND is not set in environment variables.');
             return res.status(500).json({ error: 'GAMMU_COMMAND is not set in environment variables.' });
         }
 
-        const command = `${process.env.GAMMU_COMMAND} TEXT ${destinationNumber} -text "${message}"`;
+        const command = `${process.env.GAMMU_COMMAND} sendsms TEXT ${destinationNumber} -text "${message}"`;
 
         exec(command, (err, stdout, stderr) => {
             if (err) {
+                logger.error(`Execution Error: ${err.message}`);
                 return res.status(500).json({ error: err.message });
             }
 
@@ -59,5 +72,5 @@ app.post('/send_sms',
 
 const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server running on port ${PORT}`);
 });
